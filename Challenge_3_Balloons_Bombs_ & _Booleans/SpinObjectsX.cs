792a9c9255cddd745ef20b5182e9598e4da9c3a6
MYKEL7 <onyememykel@gmail.com>
2022-09-27 21:16:52 +0100

    Challenge_3_done

---
+++ Challenge_3_Balloons_Bombs_ & _Booleans/SpinObjectsX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObjectsX : MonoBehaviour
+{
+    public float spinSpeed;
+     private int zRange = 0;
+    // Update is called once per frame
+    void Update()
+     {
+        transform.Rotate(Vector3.up *spinSpeed * Time.deltaTime );
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+}
